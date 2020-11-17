f767f98b7e739ec98962aa00681fc97178738680
Nikita Kukshinski <nkstatedev@gmail.com>
2020-11-17 21:20:20 +0000

    Deployed EC2 version

--- myproject.py
+++ myproject.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, url_for, redirect
-import csv
 
 app = Flask(__name__)
 print(__name__)
@@ -13,3 +12,6 @@ def my_home():
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
